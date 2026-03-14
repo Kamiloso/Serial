738f9b71096a9276f43883d689e5a3ce6ab7afdc
Kamil <kamiloso.skoposo@gmail.com>
2026-03-14 22:33:51 +0100

    Update Main file

--- app/src/main/java/page/se3/myproject/Main.java
+++ app/src/main/java/page/se3/myproject/Main.java
@@ -1,81 +1,75 @@
 package page.se3.myproject;
 
-import java.io.*;
-import java.nio.file.*;
-
 import page.se3.serial.SerialCompiler;
-import page.se3.serial.SerialCompiler.SyntaxException;
-import page.se3.serial.SerialCompiler.OperationException;
-import page.se3.serial.SerialCompiler.Record;
 
 public class Main {
-    public static void printHex(byte[] data) {
-        for (int i = 0; i < data.length; i++) {
-            System.out.printf("%02X%s", data[i],
-                    (i + 1) % 16 == 0 ? "\n" : ((i + 1) % 4 == 0 ? "  " : " "));
+
+    // Global object that represents a compiled SRL code
+    // It is immutable after creation, so it can be safely used from
+    // different threads.
+    private static SerialCompiler serial;
+
+    private static void initializeSerial() {
+        try {
+            // 1. Initialize the compiler (without custom compressor for this example)
+            serial = new SerialCompiler("""
+                    RECORD User
+                        LET char username[16]
+                        LET int age = 18
+                        ENSURE username MATCHES "^[\\\\p{L}0-9_-]*$"
+                        ENSURE age >= 0 AND <= 120
+                    """);
+
+        } catch (SerialCompiler.SyntaxException e) {
+            System.err.println("An error occurred (syntax): " + e.getMessage());
+            e.printStackTrace();
         }
-        System.out.println();
+    }
+
+    // An example record that uses Serial-Java-Lib to serialize
+    // and deserialize objects.
+    private record User(String username, int age) {
+        private static final String RECORD_TYPE = "User";
+
+        public byte[] serialize() {
+            return serial.makeRecord(RECORD_TYPE)
+                    .setString("username", username)
+                    .setValue("age", age)
+                    .serialize();
+        }
+
+        public static User deserialize(byte[] bytes) {
+            var record = serial.makeRecord(bytes);
+            if (!RECORD_TYPE.equals(record.RECORD_TYPE)) { // checks whether deserialization was unsuccessful
+                record = serial.makeRecord(RECORD_TYPE);
+            }
+
+            return new User(
+                    record.getString("username"),
+                    record.getValue("age", Integer.class)
+            );
+        }
     }
 
     public static void main(String[] args) {
+        initializeSerial();
+
         try {
-            SerialCompiler compiler = new SerialCompiler("""
-                    STRUCT Placa
-                        LET int zl
-                        LET int gr
-                    
-                        ENSURE zl >= 0
-                        ENSURE gr >= 0 AND < 100
-                    
-                    RECORD Czlowiek
-                        LET char imie[32]
-                        LET char nazwisko[32]
-                    
-                        DEF $name-regex = "^[\\\\p{L}]*$"
-                        ENSURE imie MATCHES $name-regex
-                        ENSURE nazwisko MATCHES $name-regex
-                    
-                    RECORD Pracownik : Czlowiek
-                        LET long id
-                        LET char placowka[32]
-                        NEST Placa placa
-                    
-                        ENSURE placowka NOT EQUALS "Uniwersytet Śląski"
-                    
-                    """);
+            // Create the user record
+            User user = new User("Cameleon-123", 35);
+            System.out.println("Original user: " + user);
 
-            // Konstrukcja
-            Record placa = compiler.makeRecord("Placa")
-                    .setValue("zl", 4000)
-                    .setValue("gr", 500); // niezgodny z warunkiem, zostanie zresetowany przy deserializacji
+            // Serialize record
+            byte[] bytes = user.serialize();
+            System.out.println("Serialized to " + bytes.length + " bytes.");
 
-            Record pracownik = compiler.makeRecord("Pracownik")
-                    .setValue("id", 24L)
-                    .setString("imie", "Jarosław")
-                    .setString("nazwisko", "Kowalski123") // niezgodny z regexem, zostanie zresetowany przy deserializacji
-                    .setString("placowka", "Politechnika Śląska")
-                    .setRecord("placa", placa);
+            // Deserialize record
+            User user2 = User.deserialize(bytes);
+            System.out.println("User from bytes: " + user2);
 
-            // :: opcjonalnie, dla naprawy rekordu przed deserializacja ::
-            // pracownik.checkAndRepair();
-
-            System.out.println("\nPrzed:");
-            System.out.println(pracownik.getReport());
-
-            // Serializacja
-            byte[] bytes = pracownik.serialize();
-
-            System.out.println("\nBajty:");
-            printHex(bytes);
-
-            // Deserializacja
-            Record deserialized = compiler.makeRecord(bytes);
-
-            System.out.println("\nPo:");
-            System.out.println(deserialized.getReport());
-
-        } catch (SyntaxException | OperationException ex) {
-            ex.printStackTrace();
+        } catch (SerialCompiler.OperationException e) {
+            System.err.println("An error occurred (operation): " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
